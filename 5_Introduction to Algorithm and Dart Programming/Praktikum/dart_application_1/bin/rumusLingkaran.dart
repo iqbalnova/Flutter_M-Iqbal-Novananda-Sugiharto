2213405bd47d03496d7f976f91f84eef1b330b7f
iqbalnova <iqbalnova707@gmail.com>
2022-08-29 12:45:52 +0700

    finish task

---
+++ 5_Introduction to Algorithm and Dart Programming/Praktikum/dart_application_1/bin/rumusLingkaran.dart
@@ -0,0 +1,7 @@
+void main() {
+  double phi = 3.14;
+  double r = 5;
+  double hasil = 0;
+  hasil = phi * r * r;
+  print('Hasilnya adalah ' + hasil.toString());
+}
---
+++ 5_Introduction to Algorithm and Dart Programming/Praktikum/dart_application_1/bin/sambungKata.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  String kata1 = "Nama";
+  String kata2 = "Saya";
+  String kata3 = "Iqbal";
+
+  print('$kata1 $kata2 $kata3');
+}
