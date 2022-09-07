d1e448761604485792206f287d687ba9dd56a904
iqbalnova <iqbalnova707@gmail.com>
2022-09-07 10:48:50 +0700

    fix return function

--- 9_Collection/Praktikum/Soal1.dart
+++ 9_Collection/Praktikum/Soal1.dart
@@ -1,15 +1,15 @@
 void main(List<String> args) {
-  var value = [1, 2, 3, 4, 5];
+  List<int> value = [1, 2, 3, 4, 5];
   print('Nilai awal $value');
-  Hitung(value, 3);
+  hitung(value, 3);
 }
 
-Future<void> Hitung(List val, int multiplier) async {
-  await Future.delayed(Duration(seconds: 4), () {
-    List<int> list = [];
+Future<List<int>> hitung(List<int> val, int multiplier) async {
+  return await Future.delayed(Duration(seconds: 4), () {
+    List<int> myList = [];
     for (var a in val) {
-      list.add(a * multiplier);
+      myList.add(a * multiplier);
     }
-    print('hasil multiplier $list');
+    return myList;
   });
 }
