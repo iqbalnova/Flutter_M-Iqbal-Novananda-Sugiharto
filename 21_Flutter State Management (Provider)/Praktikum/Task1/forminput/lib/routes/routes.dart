417600e36a806ef915a3bcbe19865290673a5aa2
iqbalnova <iqbalnova707@gmail.com>
2022-10-04 13:34:45 +0700

    init

---
+++ 21_Flutter State Management (Provider)/Praktikum/Task1/forminput/lib/routes/routes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../screens/firstscreen.dart';
+// import '../screens/secondscreen.dart';
+
+class Routes extends StatelessWidget {
+  const Routes({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Routes',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FirstScreen(),
+      },
+    );
+  }
+}
